705824a05defdd8f6eaa409a88d6a7a56f4e2461
Igor Kantor <36093648+devfire@users.noreply.github.com>
2018-02-05 11:52:20 -0500

    Update removeOldElasticSearchIndices.bash

--- removeOldElasticSearchIndices.bash
+++ removeOldElasticSearchIndices.bash
@@ -2,7 +2,7 @@
 #
 #################################Description###############################################
 #This script deletes an old ElasticSearch index.
-#It computes the current date - 31 days. The index format must be YYYY.MM.DD
+#It computes the current date - N days. The index format must be YYYY.MM.DD
 ###########################################################################################
 #
 #exit in case a command fails
@@ -27,16 +27,14 @@ command -v curl >/dev/null || { echo "ERROR: curl is not in the $PATH. Exiting."
 
 #this loops through all indices matching the date specified and grabs the 3rd column,
 #which is the index name. -s makes curl "silent" suppressing the progress bar.
-#exclude audit-* from removal
 #the grep regexp is to match all lines that follow this pattern: word-YYYY.MM.DD
 for index in `curl -s "$esURL"/_cat/indices | awk '{print $3}' | grep -E '^\w+-[0-9]{4}.[0-9]{2}.[0-9]{2}'`
 do
-        #echo curl -XDELETE "$esURL"/"$index" || { echo "ERROR: $index is not found. Exiting."; exit 1; }
         #this is to strip . from the YYYY.MM.DD format, for numbers comparison
         indexDateToDelete=$(echo $index | awk -F\- '{print $2}' | tr -d ".")
 
         #this compares the cutoff date with the index date
-        #if the index date is LESS THAN the cut off date, delete it
+        #if the index date is LESS THAN the cutoff date, delete it
         if [ $indexDateToDelete -lt $dateToDelete ];
         then
                 curl -XDELETE "$esURL"/"$index" || { echo "ERROR: $index is not found. Exiting."; exit 1; }
